b38ad4e12daa89ba9b990ca8c937c52fa473376e
Asazu Taiga <taiga.asazu@icloud.com>
2020-06-15 02:52:26 +0900

    Use only need icons

--- src/main.js
+++ src/main.js
@@ -1,14 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/free-solid-svg-icons";
-import { far } from "@fortawesome/free-regular-svg-icons";
-import { fab } from "@fortawesome/free-brands-svg-icons";
+import { faQuestion, faUndo } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 Vue.config.productionTip = false;
 
-library.add(fas, far, fab);
+library.add(faQuestion, faUndo);
 Vue.component("v-fa", FontAwesomeIcon);
 
 new Vue({
